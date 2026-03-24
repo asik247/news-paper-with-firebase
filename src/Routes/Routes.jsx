1073cf96e6f43b27f868ef84261cd892b6dacdd9
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-24 15:18:12 +0600

    nested routes setup

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,16 @@
 import { createBrowserRouter } from "react-router"
 import Root from "../Root/Root"
+import Home from "../Pages/Home"
+import Registation from "../Components/Registation"
 
 const router = createBrowserRouter([
     {
         path:'/',
-        Component:Root
+        Component:Root,
+        children:[
+            {index:true,Component:Home},
+            {path:'registaion',Component:Registation}
+        ]
     }
 ])
 export default router
