e4afc4ee0587ba8d621b49088d1e4eccccf894ca
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-28 08:14:56 +0600

    markue implement

--- src/Components/LatestNews.jsx
+++ src/Components/LatestNews.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LatestNews = () => {
     return (
         <div>
-            
+            Latest
         </div>
     );
 };
--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Headder from '../Components/Headder';
+import LatestNews from '../Components/LatestNews';
 
 const HomeLayout = () => {
     return (
         <div>
+            {/* Header code start here */}
             <Headder></Headder>
+            {/* Latest News Section code start here */}
+            <section>
+                <LatestNews></LatestNews>
+            </section>
         </div>
     );
 };
