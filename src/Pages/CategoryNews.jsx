7b8c23c639dac875cab70631871331d85442ce6d
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-29 17:47:25 +0600

    all news show

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -1,18 +1,29 @@
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
-    const {id} = useParams()
+    const { id } = useParams()
     const data = useLoaderData()
     // console.log(id,data);
-    useEffect(()=>{
-        const filteredNews = data.filter(news=>news.category_id == id);
-        console.log(filteredNews);
-    },[data,id])
+    const [categoryNews, setCategoryNews] = useState([]);
+    useEffect(() => {
+        if (id == '0') {
+            setCategoryNews(data)
+            return;
+        } else if (id == '1') {
+            const filteredNews = data.filter(news => news.others.is_today_pick == true);
+            setCategoryNews(filteredNews)
+        } else {
+            const filteredNews = data.filter(news => news.category_id == id);
+            setCategoryNews(filteredNews);
+
+        }
+
+    }, [data, id])
     return (
         <div>
-            categoryNews {id}
+            Total  {categoryNews.length} News Found
         </div>
     );
 };
