032b669717d3c339e90b02686ad6033fe7218c0e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-29 18:02:07 +0600

    find us and qzon

---
+++ src/Components/HomeLayout/FindUs.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+const FindUs = () => {
+    return (
+        <div>
+            <h2 className='font-bold mb-5'>Find us on</h2>
+            <div>
+                <div className="join join-vertical w-full">
+                    <button className="btn bg-base-100 hover:bg-base-200 justify-start join-item"><FaFacebook></FaFacebook> Facebook</button>
+                    <button className="btn bg-base-100 hover:bg-base-200 justify-start join-item"><FaTwitter></FaTwitter> Twiter</button>
+                    <button className="btn bg-base-100 hover:bg-base-200 justify-start join-item"><FaInstagram></FaInstagram> Instragram</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FindUs;
